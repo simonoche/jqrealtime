86fbebdabdd5ab1c5b31d47c45f06e1f196eef9f
Simon Lamellière <simon@gloria.local>
2013-04-20 19:26:11 +0200

    examples php

---
+++ examples/php/config.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+	$mysql = mysql_connect("localhost", "root", "");
+	mysql_select_db("jqrealtime");
+
+	// Create a session for user
+	if(!isset($_COOKIE["jqr"]))
+	{
+		$_COOKIE["jqr"] = sha1(time());
+
+		// Set the cookie
+		setcookie("jqr", $_COOKIE["jqr"], time()+360000, false, "");
+
+		// Insert USER
+		mysql_query("INSERT INTO sessions SET cookie = \"" . mysql_real_escape_string($_COOKIE["jqr"]) . "\", user_id = ((SELECT max(s2.user_id) FROM sessions s2 LIMIT 1)+1)");
+	}
+
+	// Fetch USER
+	$user = mysql_fetch_object(mysql_query("SELECT * FROM sessions WHERE cookie = \"".mysql_real_escape_string($_COOKIE["jqr"])."\""));
+
+	if(!$user)
+		die('Invalid session (flush your cookies)');
+
+?>
--- examples/php/waitformessage.php
+++ examples/php/waitformessage.php
@@ -1,3 +1,4 @@
+<?php require("config.inc.php"); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 <head>
@@ -10,6 +11,10 @@
 	</div>
 		
 	<div class="pollbar">
+
+		<div class="user_id">
+			<b>Your USER_ID is <?php echo $user->user_id; ?></b>
+		</div>
 
 		<div class="processes">
 			Current processes : <span>0</span>
@@ -31,8 +36,8 @@
 
 	</div>
 	<script type="text/javascript">
-		// Your Config
-		var poller_server = "http://localhost:8080/poll";
+		// Your Config (change it if needed)
+		var poller_server = "http://realtime.dev:8080/poll";
 	</script>
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 	<script type="text/javascript" src="js/jquery.realtime.js"></script>
